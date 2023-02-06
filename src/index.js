6207bb2dee79e232fcea232c5323d03cdd939089
Ogooezeani <ogooezeani@gmail.com>
2023-02-06 21:26:04 +0100

    Added icons and forecast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -24,6 +25,50 @@ function formatDate(date) {
   return `${dayOf} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed","Thur", "Fri", "Sat"];
+
+  return days[day];
+
+}
+
+function showForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#predict");
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) { 
+
+    if (index < 5){
+    forecastHTML = forecastHTML +
+   `
+        <div class="col-2">
+          <div class="weather-forcast-day">${formatDay(forecastDay.dt)}</div>
+          <img
+            src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+            alt=""
+            width="40"
+          />
+          <div class="weather-forcast-temps">
+            <span class="weather-forcast-temp-max"> ${Math.round(forecastDay.temp.max)}°</span>
+            <span class="weather-forcast-temp-min"> ${Math.round(forecastDay.temp.min)}°</span>
+          </div>
+        </div>
+          `;
+    }
+          });
+          forecastHTML = forecastHTML + `</div>`;
+          forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  
+  let apiLink = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
+  axios.get(apiLink).then(showForecast)
+    .catch((err) => console.log(err));
+}
+
 function updateWeather(response) {
   let cityLabel = document.querySelector("#city-label");
   cityLabel.innerHTML = response.data.name;
@@ -40,6 +85,11 @@ function updateWeather(response) {
   weather.innerHTML = response.data.weather[0].main;
   let wind = document.querySelector("#windy");
   wind.innerHTML = response.data.wind.speed;
+  let icon = document.querySelector("#weather-icon");
+  icon.src = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+
+  getForecast(response.data.coord);
+
 }
 function showcelsiusTemp(event){
   event.preventDefault();
@@ -60,17 +110,16 @@ function showfahrenheitTemp(event){
 
   let temptElement = document.querySelector("#temp");
   fahLink.classList.add("active");
-  //celLink.classList.remove("active");
+  celLink.classList.remove("active");
   let fahrenhetTempt = (celsiusTemperature * 9) / 5 + 32;
   temptElement.innerHTML = Math.round(fahrenhetTempt);
 }
 
 
-let apiKey = "616b14cbd38253313b3b8852fa77335d";
 function search(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input").value;
-  console.log(cityInput);
+  
   let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric`;
   axios
     .get(`${apiLink}&appid=${apiKey}`)
@@ -80,8 +129,7 @@ function search(event) {
 
 function getWeatherByLocation() {
   navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
+    
 
     axios
       .get(
@@ -106,5 +154,3 @@ let celLink = document.querySelector("#cel-link");
 celLink.addEventListener("click", showcelsiusTemp);
 
 getWeatherByLocation();
-
-//document.querySelector("#cel-link").classList.remove("active");
